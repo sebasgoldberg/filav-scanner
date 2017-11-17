46e9dc6b88338e73639e40ce8ee488d6b9a12575
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2017-11-17 12:32:37 -0300

    Scanner adicionado.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,35 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+], function(Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("iamsoft.filav.scanner.controller.App", {
 
 		onInit: function () {
+
+            //let viewDivId = this.getView().createId("preview");
+            //let previewElement = jQuery("#"+viewDivId)[0];
+            let previewElement = this.getView().byId('preview').getDomRef();
+
+            let scanner = new Instascan.Scanner({ 
+                video: previewElement,
+            });
+
+            scanner.addListener('scan', function (content) {
+                MessageToast.show(content);
+                console.log(content);
+            });
+
+            Instascan.Camera.getCameras().then(function (cameras) {
+                if (cameras.length > 0) {
+                    scanner.start(cameras[0]);
+                } else {
+                    console.error('No cameras found.');
+                }
+            }).catch(function (e) {
+                console.error(e);
+            });
 
 		}
 	});
